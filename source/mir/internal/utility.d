607277b97326a1b9ec7415dc6bb3b396e41bb4e5
Ilya Yaroshenko <ilyayaroshenko@gmail.com>
2017-03-07 01:59:38 +0700

    remove deprecated code

--- source/mir/internal/utility.d
+++ source/mir/internal/utility.d
@@ -3,10 +3,7 @@ module mir.internal.utility;
 import std.traits;
 import std.meta;
 
-version(Have_mir)
-    import mir.ndslice.slice;
-else
-    import std.experimental.ndslice.slice;
+import mir.ndslice.slice;
 
 alias Iota(size_t j) = Iota!(0, j);
 
@@ -36,26 +33,6 @@ template isComplex(C)
     || is(Unqual!C == cdouble)
     || is(Unqual!C == cfloat);
 }
-
-//enum isSIMDVector(V) = is(V : __vector(F[N]), F, size_t N);
-
-auto toDense(R)(Slice!(1, R) x)
-{
-    assert(x.stride == 1);
-    auto ptr = x.ptr;
-    static if (isPointer!R)
-    {
-        return ptr[0 .. x.length];
-    }
-    else
-    {
-        return ptr.range[ptr.shift .. x.length + ptr.shift];
-    }
-}
-
-alias ConstData(S : Slice!(N, T*), size_t N, T) = Slice!(N, const(T)*);
-alias ConstData(S : Slice!(N, T[]), size_t N, T) = Slice!(N, const(T)[]);
-alias ConstData(S : T[], T) = const(T)[];
 
 enum isVector(V) = is(V : T1[], T1) || is(V : Slice!(1, T2*), T2);
 enum isMatrix(M) = is(M : Slice!(2, T*), T);
